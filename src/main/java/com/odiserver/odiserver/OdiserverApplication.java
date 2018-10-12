dbb2e37d92484592e8099c44a07b77864998060f
Jiptesh Moharana <jiptesh.moharana@iddinkgroup.com>
2018-10-12 18:58:44 +0530

    OdiServer First commit

---
+++ src/main/java/com/odiserver/odiserver/OdiserverApplication.java
@@ -0,0 +1,12 @@
+package com.odiserver.odiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdiserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdiserverApplication.class, args);
+	}
+}
---
+++ src/test/java/com/odiserver/odiserver/OdiserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.odiserver.odiserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OdiserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

81da5959614834e1ccede21b2a9d15dee0d75aab
PINTU <PINTU@DESKTOP-MQFG0DI>
2019-11-14 03:23:42 +0530

    server setup and communication between micro services

--- src/main/java/com/iodia/odiserver/OdiserverApplication.java
+++ src/main/java/com/iodia/odiserver/OdiserverApplication.java
@@ -1,11 +1,12 @@
-package com.odiserver.odiserver;
+package com.iodia.odiserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class OdiserverApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OdiserverApplication.class, args);
 	}

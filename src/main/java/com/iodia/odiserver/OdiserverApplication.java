82456e4a6392243a47a76514900c83580457aeba
PINTU <PINTU@DESKTOP-MQFG0DI>
2019-11-14 04:44:43 +0530

    NetflixZuul gateway for microservices integration

--- src/main/java/com/iodia/odiserver/OdiserverApplication.java
+++ src/main/java/com/iodia/odiserver/OdiserverApplication.java
@@ -3,9 +3,11 @@ package com.iodia.odiserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableZuulServer
 public class OdiserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OdiserverApplication.class, args);
